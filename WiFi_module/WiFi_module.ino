bded40516c2908757b939770c180e8683704b038
Stefan Georgiev <tefcho.geo@gmail.com>
2024-06-10 17:14:05 +0300

    Initial commit

---
+++ WiFi_module/WiFi_module.ino
@@ -0,0 +1,114 @@
+/**
+   PostHTTPClient.ino
+
+    Created on: 21.11.2016
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+//#include <SoftwareSerial.h>
+//SoftwareSerial uartConnection(4, 3); // RX, TX
+
+
+/* this can be run with an emulated server on host:
+        cd esp8266-core-root-dir
+        cd tests/host
+        make ../../libraries/ESP8266WebServer/examples/PostServer/PostServer
+        bin/PostServer/PostServer
+   then put your PC's IP address in SERVER_IP below, port 9080 (instead of default 80):
+*/
+//#define SERVER_IP "10.0.1.7:9080" // PC address with emulation on host
+#define SERVER_IP "192.168.117.150:8081"
+
+#ifndef STASSID
+#define STASSID "Stefan"
+#define STAPSK "password"
+#endif
+
+void signa()
+{
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(200);
+}
+
+void setup() {
+  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  //uartConnection.begin(9600);
+
+  WiFi.begin(STASSID, STAPSK);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  signa();
+}
+
+
+
+void loop() {
+  // wait for WiFi connection
+  if ((WiFi.status() == WL_CONNECTED)) {
+    
+    WiFiClient client;
+    HTTPClient http;
+
+    // configure traged server and url
+    http.begin(client, "http://" SERVER_IP "/hrEntry/createEntry");  // HTTP
+    http.addHeader("Content-Type", "application/json");
+    int httpCode;
+    // httpCode = http.POST("[{\"id\":\"100\",\"value\":60}]");
+    // delay(10000)
+    String receivedString;
+    while(Serial.available()) {
+        
+        char receivedChar = Serial.read(); // Read a single character
+         // Declare a static String to accumulate characters
+
+        // Append the received character to the string
+        receivedString += receivedChar;
+
+        // Check if the received character is a newline (end of message)
+        if (receivedChar == '\n') {
+            // Process the complete string
+            signa();
+            Serial.println(receivedString);
+            httpCode = http.POST(receivedString);
+        }
+    }
+
+    // start connection and send HTTP header and body
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+
+  delay(100);
+}
---
+++ ble_central/ble_central.ino
@@ -0,0 +1,157 @@
+//последна версия на [{},{}]
+#include <ArduinoBLE.h>
+#include <ArduinoJson.h>
+#include <Vector.h>
+#define ELEMENT_COUNT_MAX 3
+
+int beats_per_min[ELEMENT_COUNT_MAX];
+String mac_adderess[ELEMENT_COUNT_MAX];
+int last = 0;
+
+void setup() {
+  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+
+    while (1);
+  }
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");//""
+  Serial1.begin(9600);  
+  Serial.println("Bluetooth® Low Energy Central - BPM reading");
+}
+
+String message="";
+int prev_millis = 0;
+
+void loop() {
+  BLEDevice peripheral = BLE.available();
+  if (peripheral) {  
+      Serial.print("Found ");
+      Serial.print(peripheral.address());
+      Serial.print(" '");
+      Serial.print(peripheral.localName());
+      Serial.print("' ");
+      Serial.print(peripheral.advertisedServiceUuid());
+      Serial.println();
+      BLE.stopScan();
+      int va = get_value(peripheral);
+      if(last<ELEMENT_COUNT_MAX)
+      {
+        beats_per_min[last] = va;
+        mac_adderess[last++] = peripheral.address();
+      }
+      peripheral.disconnect();
+      //BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+      
+  }
+
+  int current_millis = millis();
+  if(current_millis - prev_millis >= 10 * 1000)
+  {
+    prev_millis = current_millis;
+    message += "[";
+    
+    Serial.print("size = ");
+    Serial.println(last);
+    for(int i = 0 ; i < last;i++)
+    {
+      String node = zip_json(i);
+      
+      Serial.print("node = ");
+      Serial.println(node);
+      if(node=="")
+      {
+        Serial.println("Error taking!");
+        continue;
+      }
+      message+= node;
+      if( i + 1 != last)
+        message+=",";
+    }
+    message+="]";
+    Serial.println(message);
+    Serial1.println(message);
+    signa();
+    String receivedString;
+    while(Serial1.available())
+    {
+      char receivedChar = Serial1.read();
+      receivedString+=receivedChar;
+      if (receivedChar == '\n')
+      {
+        Serial.println(receivedString);
+        break;
+      }
+    }
+    last = 0;
+    message="";
+    Serial.println("-------------------------");
+    delay(500);
+    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  }
+}
+int get_value(BLEDevice peripheral)
+{
+  Serial.println("Connecting ...");
+  // Try to connect
+  if (!peripheral.connect()) {
+    Serial.println("Failed to connect!");
+    return 0;
+  }
+
+  Serial.println("Connected");
+  Serial.println("Discovering attributes ...");
+  if (!peripheral.discoverAttributes()) {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return 0;
+  }
+
+  BLECharacteristic bpmCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
+
+  if (!bpmCharacteristic) {
+    Serial.println("Peripheral does not have BPM characteristic!");
+    peripheral.disconnect();
+    return 0;
+  }
+
+  Serial.println("BPM characteristic found!");
+
+  if (peripheral.connected()) {
+    byte val = 0;
+    if (bpmCharacteristic.readValue(val))
+      return val;
+  }
+}
+
+void signa()
+{
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(150);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(150);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(150);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(150);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(150);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(150);
+}
+
+String zip_json(int i) {
+  Serial.println(i);
+  String result="";
+  result+="{\"id\":\"";
+  result+="100";
+  result+="\",\"value\":";
+  result+=beats_per_min[i];
+  result+="}";
+  Serial.println("Encoded JSON:");
+  Serial.println(result);
+  return result;
+}
